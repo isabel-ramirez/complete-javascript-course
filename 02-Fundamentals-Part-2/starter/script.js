1acc43a427f2a4be3bd7a57958d5279cba3d4cd2
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-08-27 21:34:31 -0500

    Day 1: 1hr done, videos 33 & 34

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// ACTIVATING STRICT MODE
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicense = true;
+}
+if (hasDriversLicense) {
+  console.log('I can drive :D');
+}
+
+// const interface = 'Audio';
+// const private = 342;
+*/
+
+// FUNCTIONS
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(`num is ${num}, it's a ${typeof num}!`);
