0a24ccb7a15f7b607da6a8d63cb080bf41662f3b
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-08-27 20:35:19 -0500

    fix: Add both teams lose condition for challenge 3

--- 01-Fundamentals-Part-1/starter/app.js
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -7,7 +7,7 @@ const continent = 'North America';
 let population = 342;
 
 console.log(
-    `the ${country} in ${continent} has a population of ${population} million`
+  `the ${country} in ${continent} has a population of ${population} million`
 );
 
 // 2) Data Types
@@ -45,17 +45,17 @@ console.log(description);
 // 6) Taking Decisions: if/else statement
 // population = 10;
 if (population > avgPopulation) {
-    console.log(
-        `${country}'s population is ${
-            population - avgPopulation
-        } million above average.`
-    );
+  console.log(
+    `${country}'s population is ${
+      population - avgPopulation
+    } million above average.`
+  );
 } else {
-    console.log(
-        `${country}'s population is ${
-            avgPopulation - population
-        } million below average`
-    );
+  console.log(
+    `${country}'s population is ${
+      avgPopulation - population
+    } million below average`
+  );
 }
 
 // 7) Type Conversion and Coercion
@@ -78,37 +78,37 @@ console.log(5 + 6 + '4' + 9 - 4 - 2); // -> '114' + 9 - 4 - 2 -> '114' + 3 -> '1
 
 // 8) Logical Operators
 if (language === 'English' && population < 50 && !isIsland) {
-    console.log(`You should live in ${country} :)`);
+  console.log(`You should live in ${country} :)`);
 } else {
-    console.log(`${country} does not meet your criteria :(`);
+  console.log(`${country} does not meet your criteria :(`);
 }
 
 // 9) The switch Statement
 switch (language) {
-    case 'Chinese':
-    case 'Mandarin':
-        console.log('MOST number of native speakers!');
-        break;
-    case 'Spanish':
-        console.log('2nd place in number of native speakers');
-        break;
-    case 'English':
-        console.log('3rd place in number of native speakers');
-        break;
-    case 'Hindi':
-        console.log('Number 4 in numbe rof native speakers');
-        break;
-    case 'Arabic':
-        console.log('5th most spoken language');
-        break;
-    default:
-        console.log('Great language too :D');
+  case 'Chinese':
+  case 'Mandarin':
+    console.log('MOST number of native speakers!');
+    break;
+  case 'Spanish':
+    console.log('2nd place in number of native speakers');
+    break;
+  case 'English':
+    console.log('3rd place in number of native speakers');
+    break;
+  case 'Hindi':
+    console.log('Number 4 in numbe rof native speakers');
+    break;
+  case 'Arabic':
+    console.log('5th most spoken language');
+    break;
+  default:
+    console.log('Great language too :D');
 }
 
 // 10) The Conditional (Ternary) Operator
 // population = 4;
 console.log(
-    `${country}'s population is ${population > 33 ? 'above' : 'below'} average.`
+  `${country}'s population is ${population > 33 ? 'above' : 'below'} average.`
 );
 
 console.groupEnd();
@@ -132,11 +132,11 @@ console.log(markHigherBMI);
 let output;
 
 if (bmiMark > bmiJohn) {
-    output = `Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`;
+  output = `Mark's BMI (${bmiMark}) is higher than John's (${bmiJohn})!`;
 } else if (bmiJohn < bmiMark) {
-    output = `John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`;
+  output = `John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})!`;
 } else {
-    output = `John and Mark have the same BMI (${bmiJohn}!)`;
+  output = `John and Mark have the same BMI (${bmiJohn}!)`;
 }
 
 console.log(output);
@@ -147,13 +147,17 @@ let avgKoalasScore = (109 + 95 + 106) / 3;
 const minScore = 100;
 
 if (avgDolphinScore === avgKoalasScore && avgDolphinScore >= 100) {
+  if (avgDolphinScore >= 100) {
     console.log(`TIE! both teams have a score of ${avgDolphinScore}!`);
+  } else {
+    console.log(`AWWW!! both teams lose! they scored ${avgDolphinScore}!`);
+  }
 } else if (avgDolphinScore > avgKoalasScore && avgDolphinScore >= 100) {
-    console.log(`DOPLHINS WIN! ${avgDolphinScore} to ${avgKoalasScore}`);
+  console.log(`DOPLHINS WIN! ${avgDolphinScore} to ${avgKoalasScore}`);
 } else {
-    if (avgKoalasScore >= 100) {
-        console.log(`KOALAS WIN! ${avgKoalasScore} to ${avgDolphinScore}`);
-    }
+  if (avgKoalasScore >= 100) {
+    console.log(`KOALAS WIN! ${avgKoalasScore} to ${avgDolphinScore}`);
+  }
 }
 
 // Challenge 4
@@ -162,11 +166,11 @@ const alternateTip = 0.2;
 
 let billValue = 430;
 let tipAmount =
-    billValue *
-    (billValue >= 50 && billValue <= 300 ? standardTip : alternateTip);
+  billValue *
+  (billValue >= 50 && billValue <= 300 ? standardTip : alternateTip);
 
 console.log(
-    `The bill was $${billValue}, the tip was $${tipAmount}, and the total value $${
-        billValue + tipAmount
-    }.`
+  `The bill was $${billValue}, the tip was $${tipAmount}, and the total value $${
+    billValue + tipAmount
+  }.`
 );
