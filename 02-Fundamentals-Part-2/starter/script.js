e0840e94a028935c5acae9dc19f5ff270d2f4085
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-08-29 20:34:22 -0500

    Day 3: 1h done, videos 39-41

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -83,6 +83,7 @@ function fruitProcessor(apples, oranges) {
 */
 
 // REVIEWING FUNCTIONS
+/*
 const calcAge = function (birthYear) {
   return 2037 - birthYear;
 };
@@ -101,7 +102,80 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
+*/
 
+// INTRODUCTION TO ARRAYS
+/*
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const years1 = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+// friends = ['Bob', 'Alice']; // TypeError Assignment to a constant variable
+
+const jonas = ['Jonas', 'Scmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years[years.length - 1]]);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+*/
+
+// BASIC ARRAY OPERATIONS (METHODS)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay'); // add to end of array
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John'); // add to beginning of array
+console.log(friends);
+
+// Remove elements
+friends.pop(); // remove last element
+const poppedFriend = friends.pop();
+console.log(poppedFriend);
+console.log(friends);
+
+friends.shift(); // remove first element
+console.log(friends);
+
+// Lookup
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+friends.push(23);
+console.log(friends.includes('23')); // uses strict equality, no type coercion
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend named Steven');
+}
 // CODING CHALLENGES
 // Challenge 1
 const calcAverage = (a, b, c) => (a + b + c) / 3;
