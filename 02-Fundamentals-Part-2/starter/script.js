6ea4782ec10d915a0b07f4b6b25cc8e4059eb428
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-08-30 15:00:18 -0500

    Solve challenge 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -305,3 +305,45 @@ const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 console.log(bills);
 console.log(tips);
 console.log(total);
+
+// Challenge 3
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.calcBMI()}) is higher than ${
+      john.fullName
+    }'s (${john.bmi})!`
+  );
+} else if (mark.bmi < john.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.calcBMI()}) is higher than ${
+      mark.fullName
+    }'s (${mark.bmi})!`
+  );
+} else {
+  console.log(
+    `${mark.fullName} and ${john.fullName} have the same BMI (${mark.bmi})!`
+  );
+}
