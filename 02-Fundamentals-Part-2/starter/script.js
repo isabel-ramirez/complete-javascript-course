3171aa8ae9bcd201c83a8cf6c049b6c47d53418e
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-08-31 14:38:18 -0500

    Day 5: 1h done, videos 47-50, end of section

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -222,6 +222,7 @@ console.log(
 */
 
 // OBJECT METHODS
+/*
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
@@ -265,6 +266,114 @@ console.log(jonas.age);
 console.log(jonas.age);
 
 console.log(jonas.getSummary());
+*/
+
+// ITERATION THE FOR LOOP
+/*
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+*/
+
+// LOOPING ARRAYS, BREAKING & CONTINUING
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array (one way to do it)
+  // types[i] = typeof jonas[i];
+
+  // another way to fill the types array
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+// Learned this from TOP (map method)
+console.log(years.map(year => 2037 - year));
+
+// Continue & break
+console.log('------ ONLY STRINGS ------');
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== 'string') continue;
+
+  console.log(`${jonas[i]} is a string!`);
+}
+
+console.log('------ BREAK WITH NUMBER ------');
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === 'number') break;
+
+  console.log(`${jonas[i]} is a ${typeof jonas[i]}!`);
+}
+*/
+
+// LOOPING BACKWARDS & LOOPS IN LOOPS
+/*
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+// Loop backwards
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+// Nested loops
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------ Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
+  }
+}
+*/
+
+// THE WHILE LOOP
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`FOR LOOP Lifting weights repetition ${rep}`);
+}
+
+let rep2 = 1;
+while (rep2 <= 10) {
+  console.log(`WHILE LOOP Lifting weights repetition ${rep2}`);
+  rep2++;
+}
+
+// While loop without a counter
+// roll a dice until you get a 6
+let diceRoll = Math.trunc(Math.random() * 6) + 1;
+while (diceRoll !== 6) {
+  console.log(`You rolled a ${diceRoll}!`);
+  diceRoll = Math.trunc(Math.random() * 6) + 1;
+
+  if (diceRoll === 6) {
+    console.log('You rolled a 6!! Loop is about to end :)');
+  }
+}
 
 // ASSIGNMENTS
 console.group('JS Fundamentals - Part 2 ASSIGNMENTS');
