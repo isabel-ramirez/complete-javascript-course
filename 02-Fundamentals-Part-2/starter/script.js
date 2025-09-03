7b254781e187b3852b9ea9c3d0b211270dca8c5d
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-02 23:24:00 -0500

    TOP js.info exercise 2, solve

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -739,3 +739,14 @@ const camelize = function (str) {
 console.log(camelize('list-style-image'));
 console.log(camelize('background-color'));
 console.log(camelize('-webkit-transition'));
+
+// 2) Filter range
+// filterRange([5, 3, 8, 1], 1, 4) => [3, 1]
+const filterRange = function (arr, a, b) {
+  return arr.filter(num => num >= a && num <= b);
+};
+
+const arr = [5, 3, 8, 1];
+
+console.log(filterRange(arr, 1, 4));
+console.log(arr);
