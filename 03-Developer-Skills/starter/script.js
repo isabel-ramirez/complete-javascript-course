da14f919979bb9bea5793db0dbacc658af9cfbb3
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-07 11:40:01 -0500

    Solve challenge 2

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -23,3 +23,43 @@ const test2 = [12, 5, -5, 0, 4];
 
 printForecast(test1);
 printForecast(test2);
+
+// Challenge 2
+
+const freelanceWeekSummary = function (arr) {
+  const totalHours = arr.reduce((sum, dayHours) => sum + dayHours, 0);
+  const daysWorked = arr.filter(dayHours => dayHours > 0).length;
+  const avgHours = totalHours / daysWorked;
+  const isFullTime = totalHours >= 35;
+
+  let max = arr[0];
+  let maxDay = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      maxDay = i;
+    }
+  }
+
+  const daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thurday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  const summary = `WEEKLY SUMMARY
+    Total hours worked: ${totalHours} hrs
+    Average daily hours: ${Math.round(avgHours * 10) / 10} hrs
+    Day with most hours worked: ${daysOfWeek[maxDay]}, ${arr[maxDay]} hrs 
+    Number of days worked: ${daysWorked} days
+    Full-time ${isFullTime ? 'yes' : 'no'}`;
+
+  console.log(summary);
+};
+
+const test3 = [7.5, 8, 6.5, 0, 8.5, 4, 0];
+freelanceWeekSummary(test3);
