4a4b284f627457a9a96efcebf39be106d7f216f6
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-07 17:12:58 -0500

    Implement basic logic of guessing number game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent =
+  'WOW CONGRATS THATS THE CORRECT NUMBER!! ! ٩(◕‿◕)۶';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 42140;
+
+document.querySelector('.guess').value = 17;
+console.log(document.querySelector('.guess').value);
+*/
+
+// HANDLING CLICK EVENTS
+const messageElement = document.querySelector('.message');
+const scoreElement = document.querySelector('.score');
+const numberElement = document.querySelector('.number');
+
+// Define secret number
+const secretNum = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNum);
+let score = scoreElement.textContent;
+let highScore = 0;
+// Check user's guess against secret number
+// Wrong guess: Increment score, while loop ask for another guess
+// Correct guess: change message, change color, no more input, check high score
+
+const myFunc = function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    messageElement.textContent = 'u didnt enter a number.. ᇂ_ᇂ';
+  } else if (guess === secretNum) {
+    messageElement.textContent =
+      'WOW CONGRATS THATS THE CORRECT NUMBER!! ! ⸂⸂⸜(രᴗര๑)⸝⸃⸃';
+    numberElement.textContent = secretNum;
+  } else {
+    if (guess < secretNum) {
+      messageElement.textContent = 'LMAOOO NO.. THATS TOO LOW (>▽<)';
+    } else {
+      messageElement.textContent = 'TOO HIGH oMG (´∀｀)';
+    }
+
+    score--;
+    scoreElement.textContent = score;
+    if (score === 0) {
+      messageElement.textContent = 'GAME OVER!!';
+    }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', myFunc);
